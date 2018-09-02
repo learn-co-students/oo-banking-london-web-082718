608e649d58500505bfdd18ab2448fe718fa1b8dd
klbennett <https://github.com/settings/applications>
2018-09-02 23:32:14 +0100

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,69 @@
+# # We're going to build a BankAccount class where one instance of the class can
+# transfer money to another instance through a Transfer class. The Transfer class acts as a
+# space for a transaction between two instances of the bank account class. Think of it this way: you
+# can't just transfer money to another account without the bank running checks first. Transfer instances
+# will do all of this, as well as check the validity of the accounts before the transaction occurs. Transfer
+#   instances should be able to reject a transfer if the accounts aren't valid or if the sender doesn't have the money.
+# #
+# # Transfers start out in a "pending" status. They can be executed and go to a "complete" state.
+# They can also go to a "rejected" status. A completed transfer can also be reversed and go into a "reversed" status.
+
+
+# BankAccount
+#   BankAccount
+#     #initialize
+#       can initialize a Bank Account (FAILED - 1)
+#       initializes with a name (FAILED - 2)
+#       always initializes with balance of 1000 (FAILED - 3)
+#       always initializes with a status of 'open' (FAILED - 4)
+#       can't change its name
+#   #deposit
+#     can deposit money into its account (FAILED - 5)
+#   #display_balance
+#     can display its balance (FAILED - 6)
+#   #valid?
+#     is valid with an open status and a balance greater than 0 (FAILED - 7)
+#   #close_account
+#     can close its account (FAILED - 8)
+#
+require 'pry'
 class BankAccount
+
+attr_accessor :balance, :status, :valid
+attr_reader :name
+
+@@all_accounts = []
+
+def initialize(name, balance = 1000)
+  @name = name
+  @balance = 1000
+  @status = "open"
+  @@all_accounts << self
+end
+
+def deposit(money)
+  # binding.pry
+  @balance += money
+end
+
+def balance
+  @balance
+end
+
+def display_balance
+  "Your balance is $#{@balance}."
+end
+
+def valid?
+  if @balance > 0 && @status == "open"
+    true
+  else
+    false
+  end
+end
+
+def close_account
+@status = "closed"
+end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,67 @@
+# Transfer
+#   Transfer
+#     initialize
+#       can initialize a Transfer (FAILED - 2)
+#       initializes with a sender (FAILED - 3)
+#       initializes with a receiver (FAILED - 4)
+#       always initializes with a status of 'pending' (FAILED - 5)
+#       initializes with a transfer amount (FAILED - 6)
+#     #valid?
+#       can check that both accounts are valid (FAILED - 7)
+#       calls on the sender and reciever's #valid? methods (FAILED - 8)
+#     #execute_transaction
+#       can execute a successful transaction between two accounts (FAILED - 9)
+#       each transfer can only happen once (FAILED - 10)
+#       rejects a transfer if the sender doesn't have a valid account (FAILED - 11)
+#     #reverse_transfer
+#       can reverse a transfer between two accounts (FAILED - 12)
+#       it can only reverse executed transfers (FAILED - 13)
+
+require 'pry'
 class Transfer
-  # your code here
+
+
+attr_accessor :sender, :receiver, :transfer_amount, :status, :amount, :all_transfers
+
+@@all_transfers = []
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+    @@all_transfers << self
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+  if @sender.valid? && @sender.balance > @amount && status == "pending"
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+    # binding.pry
+  else
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+end
+
+def reverse_transfer
+  @@all_transfers.select do |transaction|
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+  end
+end
+end
+
 end
